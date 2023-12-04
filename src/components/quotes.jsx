eadab03dc892955c0c1b4606b5a690ee51bc0a03
Marshanah T. ♡ <marshanah.taylor@utexas.edu>
2023-12-04 04:00:24 -0600

    update quotes file

--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'; 
 import React from "react";
 import { useNavigate } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -23,45 +24,50 @@ export default function Quotes() {
             <div>
                 <div className="fixed top-0 left-0 inset-y-0 z-50 flex w-72 flex-col">
                     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-                        <div><a href="#" className="flex h-16 shrink-0 items-center">
-                            <img className="btn h-10 w-auto" src={GroupedLogo} alt="Company Logo"></img></a>
+                        <div>
+                            <Link to="#" className="flex h-16 shrink-0 items-center">
+                                <img className="btn h-10 w-auto" src={GroupedLogo} alt="Company Logo"></img>
+                            </Link>
                         </div>
-                        <div className="icon-user-circle-wrapper"><img className="icon-user-circle" src={GroupedProfile} alt="User Profile"></img>
+                        <div className="icon-user-circle-wrapper">
+                            <img className="icon-user-circle" src={GroupedProfile} alt="User Profile"></img>
                         </div>
                         <nav className="flex flex-1 flex-col">
                             <ul role="list" className="flex flex-1 flex-col gap-y-7">
                                 <li>
                                     <ul role="list" className="-mx-2 space-y-1">
                                         <li>
-                                            <a href="chat.html"
-                                                className="btn text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
-                                                <img className="btn h-7 w-auto" src={Althea} alt="Crystal Ball"></img>
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="quotes.html"
+                                            <Link to="chat.html"
                                                 className="bg-gray-800 text-white group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
-                                                <img className="btn h-7 w-auto" src={QuotesIcon} alt="Quotes Icon"></img>
-                                            </a>
+                                                <img className="btn h-7 w-auto" src={Althea} alt="Crystal Ball"></img>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="bookmark.html"
+                                            <Link to="quotes.html"
+                                                className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
+                                                <img className="btn h-7 w-auto" src={QuotesIcon} alt="Quotes Icon"></img>
+                                            </Link>
+                                        </li>
+                                        <li>
+                                            <Link to="bookmark.html"
                                                 className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={BookmarksIcon} alt="Bookmark Icon"></img>
-                                            </a>
+                                            </Link>
                                         </li>
                                         <li>
-                                            <a href="profilesettings.html"
+                                            <Link to="profilesettings.html"
                                                 className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={ProfileSettingsIcon} alt="Cog Icon"></img>
-                                            </a>
+                                            </Link>
                                         </li>
                                     </ul>
                                 </li>
                             </ul>
                         </nav>
-                        <div> <a href="#" className="flex justify-center items-center">
-                            <img className="btn h-7 w-auto" src={SignOutIcon} alt="Sign Out Icon"></img></a>
+                        <div>
+                            <Link to="signout.html" className="flex justify-center items-center">
+                                <img className="btn h-7 w-auto" src={SignOutIcon} alt="Sign Out Icon"></img>
+                            </Link>
                         </div>
                     </div>
                 </div>
