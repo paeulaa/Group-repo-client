035a73df0ddb63eee9b0f66874c8fc62310178af
jady001 <41458731+skrud4268@users.noreply.github.com>
2023-12-04 11:16:54 -0600

    connect quotes pages

--- src/App.js
+++ src/App.js
@@ -5,26 +5,27 @@ import Main from "./components/main";
 import MainLogin from "./components/main-login";
 import Login from "./components/login";
 import Register from "./components/register";
+import Quotes from "./components/quotes";
 import Signout from "./components/signout";
 import Settings from "./components/settings";
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-    return (
-      <div className="App">
-        <Routes>
-          <Route exact path="/" element={<Main/>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          {/* <Route path="/quotes" element={<Quotes />} />
-          <Route path="/chat" element={<Chat />} /> */}
-          <Route path="/signout" element={<Signout />} />
-          <Route exact path="/settings" element={<Settings />} />
-          <Route exact path="/user" element={<MainLogin />} />
-        </Routes>
-      </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/quotes" element={<Quotes />} />
+        {/*<Route path="/chat" element={<Chat />} /> */}
+        <Route path="/signout" element={<Signout />} />
+        <Route exact path="/settings" element={<Settings />} />
+        <Route exact path="/user" element={<MainLogin />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
