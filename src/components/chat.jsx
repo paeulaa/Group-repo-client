e0a4568118dc0eb145df0d721a1d444f85a4ce8f
paeulaa <paeulaa1999@gmail.com>
2023-12-05 16:27:49 -0600

    link chat-page's meny

--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -116,11 +116,10 @@ export default function Chat() {
             <div>
                 <div className="fixed top-0 left-0 inset-y-0 z-50 flex w-72 flex-col">
                     <div className="flex grow flex-col gap-y-5 overflow-y-auto bg-gray-900 px-6">
-                        <div>
-                            <Link to="#" className="flex h-16 shrink-0 items-center">
-                                <img className="btn h-10 w-auto" src={GroupedLogo} alt="Company Logo"></img>
-                            </Link>
-                        </div>
+                        
+                        <Link to="/user" className="flex h-16 shrink-0 items-center">
+                            <img className="btn h-10 w-auto" src={GroupedLogo} alt="Company Logo"></img>
+                        </Link>
                         <div className="icon-user-circle-wrapper">
                             <img className="icon-user-circle" src={GroupedProfile} alt="User Profile"></img>
                         </div>
@@ -129,25 +128,25 @@ export default function Chat() {
                                 <li>
                                     <ul role="list" className="-mx-2 space-y-1">
                                         <li>
-                                            <Link to="chat.html"
+                                            <Link to="/chat"
                                                 className="bg-gray-800 text-white group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={Althea} alt="Crystal Ball"></img>
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link to="quotes.html"
+                                            <Link to="/quotes"
                                                 className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={QuotesIcon} alt="Quotes Icon"></img>
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link to="bookmark.html"
+                                            <Link to="/bookmarks"
                                                 className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={BookmarksIcon} alt="Bookmark Icon"></img>
                                             </Link>
                                         </li>
                                         <li>
-                                            <Link to="profilesettings.html"
+                                            <Link to="/settings"
                                                 className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold">
                                                 <img className="btn h-7 w-auto" src={ProfileSettingsIcon} alt="Cog Icon"></img>
                                             </Link>
@@ -157,7 +156,7 @@ export default function Chat() {
                             </ul>
                         </nav>
                         <div>
-                            <Link to="signout.html" className="flex justify-center items-center">
+                            <Link to="/signout" className="flex justify-center items-center">
                                 <img className="btn h-7 w-auto" src={SignOutIcon} alt="Sign Out Icon"></img>
                             </Link>
                         </div>
